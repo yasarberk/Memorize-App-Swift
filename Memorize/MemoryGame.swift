7c832516c0ebf68c273b26a51fb40c6bddb19872
yasarberk <berkyasar01@gmail.com>
2025-09-25 16:31:46 +0200

    Second part of the Lecture 5. Lecture 5 is done

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> where CardContent: Equatable{
-    private(set) var cards: Array<Card>
-    
+struct MemoryGame<CardContent> where CardContent: Equatable {
+    private(set) var cards: [Card]
+
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = []
         // add numberOfPairsOfCards x 2 cards
@@ -19,34 +19,59 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
             cards.append(Card(id: "\(pairIndex + 1) b", content: content))
         }
     }
-    
+
     mutating func shuffle() {
         cards.shuffle()
         print(cards)
     }
-    
-    mutating func choose(_ card: Card) {
-        let chosenindex = index(of: card)
-        cards[chosenindex].isFaceUp.toggle()
+
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get { return cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) }}
     }
-    
-    func index(of card: Card) -> Int {
+
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chosenIndex].content
+                        == cards[potentialMatchIndex].content
+                    {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
+    }
+
+    private func index(of card: Card) -> Int? {
         for index in cards.indices {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0 // FIXME: fake
+        return nil
     }
+
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
         var id: String
-        
-        var isFaceUp = true
+
+        var isFaceUp = false
         var isMatched = false
         var content: CardContent
-        
+
         var debugDescription: String {
             "\(id): \(content) \(isFaceUp ? "face up" : "face down") \(isMatched ? "matched" : "unmatched")"
         }
+    }
+}
+
+extension Array {
+    var only:Element? {
+        return count == 1 ? first: nil
     }
 }
