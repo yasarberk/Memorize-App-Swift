bb75df4909ca760109347c7f9c572009d8c9cb5b
yasarberk <berkyasar01@gmail.com>
2025-09-27 22:41:27 +0200

    Lecture 6

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Berk Yaşar on 27.09.2025.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
