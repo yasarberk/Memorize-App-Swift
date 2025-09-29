2a87d2e20f08126bdc48274fc715628d616d6000
yasarberk <berkyasar01@gmail.com>
2025-09-29 16:14:37 +0200

    Lecture 7 first 15 minutes

---
+++ Memorize/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Berk Yaşar on 29.09.2025.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
